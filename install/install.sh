dd7ec15a552a7ce0ab2835c48bd85d015a937173
sevenpi <31717037+sevenpi@users.noreply.github.com>
2017-09-07 22:07:27 -0700

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,1 +1,10 @@
+#!/bin/bash  
+
+#------------------------------------------------------------------------------
+#
+# Perform full system in Ubuntu after fresh installation.
+# Expects to run as Superuser.
+#
+#------------------------------------------------------------------------------
+
 apt-get -y update && apt-get -y upgrade && apt-get -y dist-upgrade && apt-get -y install -f
