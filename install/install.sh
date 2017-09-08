f40f71a3cf41fbe02bb76994ea77ea1a6e928273
sevenpi <31717037+sevenpi@users.noreply.github.com>
2017-09-07 22:20:11 -0700

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -7,4 +7,8 @@
 #
 #------------------------------------------------------------------------------
 
-apt-get -y update && apt-get -y upgrade && apt-get -y dist-upgrade && apt-get -y install -f
+# Preliminary update.
+apt-get -y update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get -y install -f
